840303cebbf701bf48659dadb2a5319623dbeeb7
pennam <m.pennasilico@arduino.cc>
2024-02-05 09:33:12 +0100

    Add LICENSE file and fix file licensing

--- src/SecureElement.h
+++ src/SecureElement.h
@@ -1,20 +1,11 @@
 /*
-  SecureElement.h
-  Copyright (c) 2023 Arduino SA.  All right reserved.
+  This file is part of the Arduino_SecureElement library.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+  Copyright (c) 2024 Arduino SA
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #ifndef SECURE_ELEMENT_H_
